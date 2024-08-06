59adff50946e7704e451b0be16eea7a23e1ad385
Ogwu Emmanuel Berit <beritogwu@gmail.com>
2024-08-06 02:43:05 +0100

    update unit test

--- app/_modules/products/presentation/atoms/AddToCartButton/product.mock.ts
+++ app/_modules/products/presentation/atoms/AddToCartButton/product.mock.ts
@@ -3,9 +3,7 @@ import { Product } from "../../../domain/product";
 export const productMock: Product = {
   name: "iPhone 6",
   description: "mobile phone",
-  currencyCode: "USD",
-  currencySymbol: "$",
   price: 50,
-  quantity: 500,
-  imageLocation: "image.com",
+  image: "/smartphone.jpg",
+  product_id: 1,
 };
